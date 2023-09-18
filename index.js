20fd286cb0174460d406e6fb832dcef8141d8cc1
seannyandusi <seanmotanya@gmail.com>
2023-09-18 17:54:33 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,2 +1,15 @@
 
 // Write your code here
+let num1 = 4;
+let num2 = 15.5;
+
+let multiply = num1*num2;
+
+let random = Math.floor(Math.random()*Number.MAX_SAFE_INTEGER) +1;
+
+let num3 = 19;
+let num4 = 5;
+
+let mod = num3%num4;
+
+ let max = Math.max(8,7,15,19,20);
